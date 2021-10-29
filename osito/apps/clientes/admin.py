2eb6c1050ebfa88e29d36e8947aee9c9f7b45c21
YilberthAndres <yilberthgalarza444@gmail.com>
2021-10-29 14:25:24 -0500

    yilberth

--- osito/apps/clientes/admin.py
+++ osito/apps/clientes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.clientes.models import Cliente
 
 # Register your models here.
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display  = ('nombre','apellidos','direccion','telefono')
+    ordering      = ('nombre','apellidos')
+    search_fields = ('nombre','apellidos')
+
+
+admin.site.register(Cliente, ClienteAdmin)
--- osito/apps/clientes/models.py
+++ osito/apps/clientes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+    nombre    = models.CharField(max_length=100, verbose_name="Nombre")
+    apellidos = models.CharField(max_length=100, verbose_name="Apellido")
+    direccion = models.CharField(max_length=100, verbose_name="Direccion")
+    telefono  = models.CharField(max_length=100, verbose_name="Telefono")
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name = "Clientes"
