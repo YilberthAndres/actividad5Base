1f5b369aa497b9e3b4af69890c7d34dd514d0505
leinys <lepagifo@hotmail.com>
2021-10-31 21:24:36 -0500

    leinys

--- osito/osito/settings.py
+++ osito/osito/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.clientes.apps.ClientesConfig',
-    'apps.productos.apps.ProductosConfig'
+    'apps.productos.apps.ProductosConfig',
+    'apps.proveedores.apps.ProveedoresConfig'
 ]
 
 MIDDLEWARE = [
