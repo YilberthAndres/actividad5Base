446d35eed369a4ffab7fd2c108da2ae9e7705356
leinys <lepagifo@hotmail.com>
2021-10-31 21:33:03 -0500

    leinys

--- osito/apps/proveedores/models.py
+++ osito/apps/proveedores/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from apps.productos.models import Producto
 
 # Create your models here.
+class Proveedore(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido= models.CharField(max_length=100)
+    direccion = models.CharField(max_length=100)
+    provincia = models.CharField(max_length=100)
+    telefono = models.IntegerField(max_length=12)
+    productos = models.ManyToManyField(Producto, through= 'Distribuidos')
+    
+    
+ 
+    def __str__(self):
+        return self.nombre
+
+
+class Distribuidos(models.Model):
+    proveedore = models.ForeignKey(Proveedore, on_delete =models.CASCADE,verbose_name="proveedor" )
+    producto = models.ForeignKey(Producto, on_delete= models.CASCADE,verbose_name="producto")
+    cantidadProducto = models.IntegerField()     
